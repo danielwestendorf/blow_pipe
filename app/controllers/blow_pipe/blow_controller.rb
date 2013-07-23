6bae168bbd44a490a7e8fb10d509dcad3ecd16a1
Daniel Westendorf <daniel.westendorf@push-it-forward.com>
2013-07-23 08:57:22 -0600

    Reload fixtures if the strategy isn't transactional

--- app/controllers/blow_pipe/blow_controller.rb
+++ app/controllers/blow_pipe/blow_controller.rb
@@ -1,9 +1,16 @@
 require_dependency "blow_pipe/application_controller"
+require 'active_record/fixtures'
 
 module BlowPipe
   class BlowController < ApplicationController
     def it
       DatabaseCleaner.clean
+      if BlowPipe.configuration.strategy != :transaction
+        $fixtures ||= Dir[Rails.root.join('test', 'fixtures', '*.yml')].collect {|f| f.split('/')[-1][0..-5]}
+        $fixtures.each do |fixtures|
+          ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), fixture)
+        end
+      end
       render text: 'true', status: 200
     end
   end
